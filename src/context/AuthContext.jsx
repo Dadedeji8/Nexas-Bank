da1ccbb0914719585884e25df3eddfc2dad08e5e
Dadedeji8 <97545459+Dadedeji8@users.noreply.github.com>
2025-04-21 13:36:25 +0100

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -10,7 +10,7 @@ const AuthenticationContext = createContext();
  */
 export const AuthProvider = ({ children }) => {
 
-    const endpoint = "https://Nexeas-bank-seven.vercel.app/api";
+    const endpoint = "https://nexas-bank-seven.vercel.app/api";
 
     const [isAuthenticated, setIsAuthenticated] = useState(false);
 
